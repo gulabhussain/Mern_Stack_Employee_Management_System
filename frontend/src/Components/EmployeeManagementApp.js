24827cd37fdea406e231b6d7238521c38aabb2cd
Gulab Hussain <gulabhussain2033@gmail.com>
2026-04-23 23:42:37 +0530

    Refactor EmployeeManagementApp component

--- frontend/src/Components/EmployeeManagementApp.js
+++ frontend/src/Components/EmployeeManagementApp.js
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import EmployeeTable from './EmployeeTable';
 import AddEmployee from './AddEmployee';
-import { DeleteEmployeeById, GetAllEmployees } from '../api';
+import { GetAllEmployees } from '../api';
 import { ToastContainer } from 'react-toastify';
-import { notify } from '../utils';
-
 
 const EmployeeManagementApp = () => {
     const [showModal, setShowModal] = useState(false);
-    const [employeeObj, setEmployeeObj] = useState(null)
+    const [employeeObj, setEmployeeObj] = useState(null);
+
     const [employeesData, setEmployeesData] = useState({
         employees: [],
         pagination: {
@@ -20,37 +19,43 @@ const EmployeeManagementApp = () => {
     });
 
     const fetchEmployees = async (search = '', page = 1, limit = 5) => {
-        console.log('Called fetchEmployees')
         try {
-            const data =
-                await GetAllEmployees(search, page, limit);
-            console.log(data);
+            const data = await GetAllEmployees(search, page, limit);
             setEmployeesData(data);
         } catch (err) {
-            alert('Error', err);
+            console.log(err);
+            alert('Error fetching employees');
         }
-    }
+    };
+
     useEffect(() => {
         fetchEmployees();
-    }, [])
-
+    }, []);
 
     const handleSearch = (e) => {
-        fetchEmployees(e.target.value)
-    }
+        fetchEmployees(e.target.value);
+    };
 
-    const handleUpdateEmployee = async (emp) => {
+    const handleUpdateEmployee = (emp) => {
         setEmployeeObj(emp);
         setShowModal(true);
-    }
+    };
+
     return (
         <div className='d-flex flex-column justify-content-center align-items-center w-100 p-3'>
             <h1>Employee Management App</h1>
+
             <div className='w-100 d-flex justify-content-center'>
                 <div className='w-80 border bg-light p-3' style={{ width: '80%' }}>
+                    
                     <div className='d-flex justify-content-between mb-3'>
-                        <button className='btn btn-primary'
-                            onClick={() => setShowModal(true)}>Add</button>
+                        <button
+                            className='btn btn-primary'
+                            onClick={() => setShowModal(true)}
+                        >
+                            Add
+                        </button>
+
                         <input
                             onChange={handleSearch}
                             type="text"
@@ -58,6 +63,7 @@ const EmployeeManagementApp = () => {
                             className='form-control w-50'
                         />
                     </div>
+
                     <EmployeeTable
                         employees={employeesData.employees}
                         pagination={employeesData.pagination}
@@ -73,6 +79,7 @@ const EmployeeManagementApp = () => {
                     />
                 </div>
             </div>
+
             <ToastContainer
                 position='top-right'
                 autoClose={3000}
